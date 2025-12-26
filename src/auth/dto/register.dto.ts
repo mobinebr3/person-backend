c0bd8c7dddc1f576b888f4d6935489d42a22ba86
MehrshadSB <mehrshadsaebi.14@gmail.com>
2025-12-26 18:30:53 +0330

    add swagger

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,15 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({type: String , format: "email"})
   @IsEmail({}, {message: "ایمیل وارد شده معتبر نیست"})
   @IsNotEmpty({message: "ایمیل نمیتواند خالی باشد"})
   email: string;
 
+  @ApiProperty({type: String , format: "password", minimum: 8, required: true})
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
   password: string;
 
+  @ApiProperty({type: String , required: true})
   @IsString()
   @IsNotEmpty()
   name: string;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import { AppModule } from './app.module.js';
@@ -16,6 +17,14 @@ async function bootstrap() {
   }));
   app.useGlobalInterceptors(new TransformInterceptor(new Reflector()))
 
+ const config = new DocumentBuilder()
+    .setTitle('perseo docs')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('Perseo')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
